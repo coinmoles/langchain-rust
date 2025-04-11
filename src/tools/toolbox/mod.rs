534b36eee4f6daec327b69194dc571e5f490c7ed
coinmoles <andrewpark421@gmail.com>
2025-04-11 08:52:10 +0000

    feat: implement simple toolbox for local tools

--- src/tools/toolbox/mod.rs
+++ src/tools/toolbox/mod.rs
@@ -4,3 +4,6 @@ pub use toolbox::*;
 
 mod list_tools;
 pub use list_tools::*;
+
+mod simple_toolbox;
+pub use simple_toolbox::*;
---
+++ src/tools/toolbox/simple_toolbox.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+use async_trait::async_trait;
+
+use crate::tools::Tool;
+
+use super::Toolbox;
+
+pub struct SimpleToolbox {
+    name: String,
+    tools: HashMap<String, Box<dyn Tool>>,
+}
+
+impl SimpleToolbox {
+    pub fn new<S>(name: S, tools: HashMap<String, Box<dyn Tool>>) -> Self
+    where
+        S: Into<String>,
+    {
+        Self {
+            name: name.into(),
+            tools,
+        }
+    }
+}
+
+#[async_trait]
+impl Toolbox for SimpleToolbox {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    async fn get_tools(
+        &self,
+    ) -> Result<&HashMap<String, Box<dyn Tool>>, Box<dyn std::error::Error + Send + Sync>> {
+        Ok(&self.tools)
+    }
+}
