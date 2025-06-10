43ac64f92e996bd89c52e3e96d13b638d4408056
coinmoles <andrewpark421@gmail.com>
2025-06-10 01:13:46 +0000

    feat(ctor): prevent manual implementation of InputCtor and OutputCtor

--- src/schemas/io_model/ctor.rs
+++ src/schemas/io_model/ctor.rs
@@ -2,27 +2,35 @@ pub use macros::Ctor;
 
 use crate::schemas::ChainInput;
 
+mod sealed {
+    pub trait Sealed {}
+}
+
 pub trait Ctor: Send + Sync + 'static {
     type Target<'a>: Send + Sync + 'a;
 }
 
-pub trait InputCtor: Send + Sync + 'static {
+pub trait InputCtor: sealed::Sealed + Send + Sync + 'static {
     type Target<'a>: ChainInput;
 }
 
-pub trait OutputCtor: Send + Sync + 'static {
+pub trait OutputCtor: sealed::Sealed + Send + Sync + 'static {
     type Target<'a>: Send + Sync + 'a;
 }
 
-impl<T: Ctor> InputCtor for T
+impl<T: Ctor> sealed::Sealed for T {}
+
+impl<T> InputCtor for T
 where
+    T: Ctor + sealed::Sealed,
     for<'a> T::Target<'a>: ChainInput,
 {
     type Target<'a> = T::Target<'a>;
 }
 
-impl<T: Ctor> OutputCtor for T
+impl<T> OutputCtor for T
 where
+    T: Ctor + sealed::Sealed,
     for<'a> T::Target<'a>: Send + Sync + 'a,
 {
     type Target<'a> = T::Target<'a>;
