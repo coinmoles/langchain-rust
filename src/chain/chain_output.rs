457a00ec0806740c803de853510382e1f145e825
coinmoles <andrewpark421@gmail.com>
2025-07-03 11:05:07 +0000

    feat: implement `PureOutput` for owned output that do not require input

--- src/chain/chain_output.rs
+++ src/chain/chain_output.rs
@@ -1,6 +1,11 @@
 pub use macros::ChainOutput;
+use serde::de::DeserializeOwned;
 
-use crate::{output_parser::OutputParseError, schemas::ToolCall};
+use crate::{
+    chain::Ctor,
+    output_parser::{parse_partial_json, OutputParseError},
+    schemas::ToolCall,
+};
 
 pub trait ChainOutput<I>: Sized + Send + Sync {
     fn construct_from_text(_text: impl Into<String>) -> Result<Self, OutputParseError> {
@@ -33,4 +38,31 @@ impl<T> ChainOutput<T> for String {
     fn construct_from_text(output: impl Into<String>) -> Result<Self, OutputParseError> {
         Ok(output.into())
     }
+}
+
+pub struct PureOutput<O: DeserializeOwned + Send + Sync + 'static>(pub O);
+
+impl<O: DeserializeOwned + Send + Sync + 'static> PureOutput<O> {
+    pub fn into_inner(self) -> O {
+        self.0
+    }
+
+    pub fn as_inner(&self) -> &O {
+        &self.0
+    }
+}
+
+impl<T, O: DeserializeOwned + Send + Sync> ChainOutput<T> for PureOutput<O> {
+    fn construct_from_text(output: impl Into<String>) -> Result<Self, OutputParseError> {
+        let original: String = output.into();
+        let value = parse_partial_json(&original, false)?;
+        let deserialized = serde_json::from_value::<O>(value)?;
+        Ok(PureOutput(deserialized))
+    }
+}
+
+pub struct PureOutputCtor<O: DeserializeOwned + Send + Sync>(std::marker::PhantomData<O>);
+
+impl<O: DeserializeOwned + Send + Sync + 'static> Ctor for PureOutputCtor<O> {
+    type Target<'a> = PureOutput<O>;
 }
