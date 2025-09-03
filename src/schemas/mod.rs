7f68f572935a172c4cd986b3cef3e1afe8a40c36
Jiho Park <andrewpark421@gmail.com>
2025-09-03 06:49:22 +0000

    chore: remove unused `step_func`

--- src/schemas/mod.rs
+++ src/schemas/mod.rs
@@ -16,9 +16,6 @@ pub use retrievers::*;
 pub mod streaming_func;
 pub use streaming_func::*;
 
-pub mod step_func;
-pub use step_func::*;
-
 mod stream;
 pub use stream::*;
 
--- src/schemas/step_func.rs
+++
@@ -1,9 +0,0 @@
-use std::{error::Error, future::Future, pin::Pin};
-
-use crate::agent::AgentStep;
-
-pub type OnStepFunc = dyn FnMut(
-        &AgentStep,
-    ) -> Pin<Box<dyn Future<Output = Result<(), Box<dyn Error + Send + Sync>>> + Send>>
-    + Send
-    + Sync;
