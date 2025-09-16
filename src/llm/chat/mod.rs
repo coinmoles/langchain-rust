85e4cea925b9a2223efb0f5079ee218afedef82b
Jiho Park <andrewpark421@gmail.com>
2025-09-16 08:08:29 +0000

    chore: rename chat modules

--- src/llm/chat/mod.rs
+++ src/llm/chat/mod.rs
@@ -1,14 +1,14 @@
-mod openai_client;
-pub use openai_client::*;
+mod openai_chat;
+pub use openai_chat::*;
 
-mod generic_client;
-pub use generic_client::*;
+mod generic_chat;
+pub use generic_chat::*;
 
-mod openai_builder;
-pub use openai_builder::*;
+mod openai_chat_builder;
+pub use openai_chat_builder::*;
 
-mod generic_builder;
-pub use generic_builder::*;
+mod generic_chat_builder;
+pub use generic_chat_builder::*;
 
 mod request;
 pub use request::*;
