e0120ff5d349206e30600bc4ed9a5a068c2fbdf2
Jiho Park <andrewpark421@gmail.com>
2025-09-25 06:04:06 +0000

    docs: add doc comments for `GeneriChat` and its builder

--- src/llm/chat/generic_chat.rs
+++ src/llm/chat/generic_chat.rs
@@ -15,14 +15,58 @@ use crate::llm::{
 };
 use crate::schemas::{FunctionSpec, IntoWithUsage, Message, Prompt, Role, ToolSpec, WithUsage};
 
-pub struct GenericChat<C: Config> {
+/// The wrapper for generic chat models that support OpenAI API.
+///
+/// This struct implements tool use by prompting the model to emit tool calls as plain text and
+/// manually parsing them. The specific prompt and parsing logic is injected via the [`Instructor`]
+/// object.
+///
+/// While this works with any OpenAI-comptaible model. However, for models that support structured
+/// tool call, it is recommended to use [`OpenAIChat`](crate::llm::OpenAIChat) for better
+/// reliability.
+pub struct GenericChat<C: Config = OpenAIConfig> {
+    /// The OpenAI client.
     client: OpenAIClient<C>,
+    /// The model id.
     model: String,
+    /// The instructor used to create tool use instruction and parse tool calls.
     instructor: Box<dyn Instructor>,
+    /// The call options.
     call_options: CallOptions,
 }
 
+impl<C: Config + Default> GenericChat<C> {
+    /// Creates a [`GenericChatBuilder`] to configure a [`GenericChat`].
+    ///
+    /// This is the same as calling [`GenericChatBuilder::new`].
+    ///
+    /// # Example
+    /// ```rust
+    /// use langchain_rust::llm::{GenericChat, GenericChatBuilder};
+    ///
+    /// let chat: GenericChat = GenericChat::builder().with_model("gpt-4o-mini").build();
+    /// ```
+    #[must_use]
+    pub fn builder() -> GenericChatBuilder<C> {
+        GenericChatBuilder::new()
+    }
+}
+
 impl<C: Config> GenericChat<C> {
+    /// Constructs a new [`GenericChat`]
+    ///
+    /// # Example
+    /// ```rust
+    /// use langchain_rust::llm::{DefaultInstructor, GenericChat, OpenAIModel};
+    ///
+    /// let chat: GenericChat = GenericChat::new(
+    ///     async_openai::Client::new(),
+    ///     OpenAIModel::Gpt4oMini,
+    ///     Box::new(DefaultInstructor),
+    ///     Default::default(),
+    /// );
+    /// ```
+    #[must_use]
     pub fn new<S>(
         client: OpenAIClient<C>,
         model: S,
@@ -40,12 +84,19 @@ impl<C: Config> GenericChat<C> {
         }
     }
 
+    /// Process the prompt into messages.
+    ///
+    /// The processing includes:
+    /// - Appending the tool use instruction into the first system message.
+    /// - Converting system messages to ai messages if configured.
+    /// - Converting tool call/result messages to normal ai/human messages.
+    /// - Dropping the thought part of tool call messages if configured.
     fn process_prompt(&self, prompt: Prompt, tools: Option<&[FunctionSpec]>) -> Vec<Message> {
         prompt
             .to_messages()
             .into_iter()
             .scan(true, |first_system, mut message| {
-                // Inject tool instruction into the first system message.
+                // Append tool use instruction into the first system message.
                 if *first_system && message.role == Role::System {
                     if let Some(tools) = tools {
                         let instruction = self.instructor.tool_use_instruction(tools);
@@ -62,6 +113,7 @@ impl<C: Config> GenericChat<C> {
                 // Convert tool call/result messages to normal ai/human messages
                 if let Some(tool_calls) = message.tool_calls {
                     if self.call_options.drop_thought && !tool_calls.is_empty() {
+                        // Drop the thought part.
                         message.content = String::new()
                     } else {
                         writeln!(message.content).expect("`Write` to `String` never fails");
@@ -81,12 +133,6 @@ impl<C: Config> GenericChat<C> {
                 Some(message)
             })
             .collect::<Vec<_>>()
-    }
-}
-
-impl<C: Config + Default> GenericChat<C> {
-    pub fn builder() -> GenericChatBuilder<C> {
-        GenericChatBuilder::default()
     }
 }
 
--- src/llm/chat/generic_chat_builder.rs
+++ src/llm/chat/generic_chat_builder.rs
@@ -4,21 +4,31 @@ use reqwest::Client;
 
 use super::GenericChat;
 use crate::llm::options::CallOptions;
-use crate::llm::{DefaultInstructor, Instructor};
+use crate::llm::{DefaultInstructor, Instructor, OpenAIModel};
 
+/// A builder for constructing a [`GenericChat`].
 pub struct GenericChatBuilder<C: Config> {
-    pub api_config: C,
-    pub model: String,
-    pub instructor: Box<dyn Instructor>,
-    pub call_options: CallOptions,
-    pub http_client: Option<Client>,
+    /// The HTTP client to use.
+    http_client: Option<Client>,
+    /// The API configuration.
+    api_config: C,
+    /// The model id.
+    model: String,
+    /// The [`Instructor`] used to create tool use instruction and parse tool calls.
+    instructor: Box<dyn Instructor>,
+    /// The call options.
+    call_options: CallOptions,
 }
 
-impl<C: Config + Default> Default for GenericChatBuilder<C> {
-    fn default() -> Self {
+impl<C: Config + Default> GenericChatBuilder<C> {
+    /// Creates a new [`GenericChatBuilder`].
+    ///
+    /// This is the same as calling [`GenericChat::builder`].
+    #[must_use]
+    pub fn new() -> Self {
         GenericChatBuilder {
             api_config: C::default(),
-            model: "gpt-3.5-turbo".to_string(),
+            model: OpenAIModel::Gpt4oMini.to_string(),
             instructor: Box::new(DefaultInstructor),
             call_options: CallOptions::default(),
             http_client: None,
@@ -27,41 +37,52 @@ impl<C: Config + Default> Default for GenericChatBuilder<C> {
 }
 
 impl<C: Config> GenericChatBuilder<C> {
-    pub fn with_api_config(mut self, api_config: C) -> Self {
-        self.api_config = api_config;
-        self
-    }
-
-    pub fn with_model<S: Into<String>>(mut self, model: S) -> Self {
-        self.model = model.into();
-        self
-    }
-
-    pub fn with_instructor(mut self, instructor: impl Instructor + 'static) -> Self {
-        self.instructor = Box::new(instructor);
-        self
-    }
-
-    pub fn with_call_options(mut self, call_options: CallOptions) -> Self {
-        self.call_options = call_options;
-        self
-    }
-
+    /// Sets the HTTP client to use.
     pub fn with_http_client(mut self, http_client: Client) -> Self {
         self.http_client = Some(http_client);
         self
     }
 
+    /// Sets the API configuration.
+    pub fn with_api_config(mut self, api_config: C) -> Self {
+        self.api_config = api_config;
+        self
+    }
+
+    /// Sets the model id.
+    ///
+    /// You can use the predefined models (e.g.,
+    /// [`OpenAIModel::Gpt4oMini`](crate::llm::OpenAIModel::Gpt4oMini)) or provide a custom model
+    /// name with a raw string.
+    pub fn with_model(mut self, model: impl Into<String>) -> Self {
+        self.model = model.into();
+        self
+    }
+
+    /// Sets the instructor.
+    ///
+    /// Instructor is used to used to create tool use instruction and parse tool calls.
+    pub fn with_instructor(mut self, instructor: impl Instructor + 'static) -> Self {
+        self.instructor = Box::new(instructor);
+        self
+    }
+
+    /// Sets the call options.
+    pub fn with_call_options(mut self, call_options: CallOptions) -> Self {
+        self.call_options = call_options;
+        self
+    }
+
+    /// Returns a [`GenericChat`] that uses this [`GenericChatBuilder`] configuration.
     pub fn build(self) -> GenericChat<C> {
-        let client = {
-            let mut client = OpenAIClient::with_config(self.api_config);
-            if let Some(http_client) = self.http_client {
-                client = client.with_http_client(http_client)
-            }
-
-            client
-        };
-
+        let http_client = self.http_client.unwrap_or_default();
+        let client = OpenAIClient::build(http_client, self.api_config, Default::default());
         GenericChat::new(client, self.model, self.instructor, self.call_options)
+    }
+}
+
+impl<C: Config + Default> Default for GenericChatBuilder<C> {
+    fn default() -> Self {
+        Self::new()
     }
 }
