59d70b6633ee5c1d974328a790339caff0ae14bb
Jiho Park <andrewpark421@gmail.com>
2025-09-29 07:03:25 +0000

    docs: add doc for `OpenAIModel`

--- src/llm/models.rs
+++ src/llm/models.rs
@@ -1,11 +1,17 @@
 use std::fmt::{self, Display};
 
+/// Helper enum for OpenAI model names.
 #[derive(Clone)]
 pub enum OpenAIModel {
+    /// `"gpt-3.5-turbo"`
     Gpt35,
+    /// `"gpt-4"`
     Gpt4,
+    /// `"gpt-4-turbo-preview"`
     Gpt4Turbo,
+    /// `"gpt-4o"`
     Gpt4o,
+    /// `"gpt-4o-mini"`
     Gpt4oMini,
 }
 
